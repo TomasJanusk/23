29d7870617c446d68a6c876b4072794623a20e4a
TomasJanusk <159446279+TomasJanusk@users.noreply.github.com>
2024-04-16 15:56:45 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,180 @@
+// let firstName = "Jonas";
+// let age =25;
+// let price = 12.56;
+// let userOnline = false;
+// console.log(firstName);
+// console.log(age);
+// console.log(price);
+// console.log(userOnline);
+
+
+// //Array
+// let users = ['jonas', 'ieva','Tadas','Karolis']
+// let user = {
+//     firstName: "Jonas",
+//     lastName:"jonaitis",
+//     email: "jonas@gmail.com"
+// }
+// console.log(users[2]);
+// console.log(typeof(users));
+// console.log(user);
+// let items = 15;
+// console.log(items++);
+
+// let userOnePoint = 10;
+// let userTwoPoints= '10'
+
+
+// let items = 12;
+// let price = 12.56;
+// console.log(`Items price is ${items*price}`);
+
+
+
+
+
+// let items = ['Phone', 'Computer', 'Milk']
+
+// console.log(items.length);
+
+// items = items.join(',');//convert array to string
+
+// items = items.split(',');//convert string to array
+
+// items.push('smart watch');//add to array
+
+// items.pop();//remove from array
+
+// items.shift();//remove first element in array
+
+// items.unshift('kazkas');//add first element to array
+
+// console.log(items);
+
+// let activUser = 'jonas';
+// if(activUser){
+//     console.log('Is active');
+// }else{
+//     console.log('user is inactive');
+// }
+
+
+
+// let language ='lt';
+//  switch(language){
+//     case "lt":
+//         console.log
+//  }                            
+
+
+
+//  for(let i=0;i<10;i++){
+//     console.log(i);
+//  }
+
+
+//  for(let item of items){
+//     console.log(item)
+//  }
+
+
+// let item = {
+//     code:1234,
+//     title:'Computer',
+//     count:12,
+//     price: 1250.45
+// };
+// for(let property in item){
+//     console.log(`property: ${item[property]}`);
+// }
+
+
+
+// let userList = [
+//     'Gabriele',
+//     'Laura',
+//     'Greta',
+//     'Egle',
+//     'Ieva',
+//     'Milda',
+//     'Jurgita',
+//     'Rasa',
+//     'Giedre',
+//     'Ingrida'
+// ];
+// for(let item of userList){
+//     if(item.length == 5 || item === 'Milda')
+//     console.log(item)
+// }
+
+
+// let students = [
+//     {
+//         id:1,
+//         firstName:'Jonas',
+//         lastName:'Jonaitis',
+//         age:22,
+//         email:'jonas@gmail.com',
+//         group: 'students'
+//     },
+//     {
+//         id:2,
+//         firstName:'Petras',
+//         lastName:'Petras',
+//         age:21,
+//         email:'petras@gmail.com',
+//         group: 'students'
+//     },
+//     {
+//         id:3,
+//         firstName:'Tomas',
+//         lastName:'Tomaitis',
+//         age:41,
+//         email:'tomas@gmail.com',
+//         group: 'students'
+//     },
+//     {
+//         id:4,
+//         firstName:'Arunas',
+//         lastName:'Arunaitis',
+//         age:25,
+//         email:'arunas@gmail.com',
+//         group: 'students'
+//     },
+//     {
+//         id:5,
+//         firstName:'Karolis',
+//         lastName:'Pavardenis',
+//         age:37,
+//         email:'karolis@gmail.com',
+//         group: 'students'
+//     },
+// ]
+
+
+
+
+// console.log('Studentu sarasas:')
+
+// for(let item of students){
+//      console.log('---------------------------------')
+//      if(item.age <= 25)
+//         for(let property in item){
+//            console.log(`${property}:${item[property]}`)
+//      }
+// } 
+
+// let firstName = prompt('Ivesk varda')
+// alert(firstName)
+
+
+
+
+let listNames = []
+let count = promt('sveciu skaicius')
+for(i=0,i<count,i++);
+   let names = promt('vardai')
+
+
+
+
